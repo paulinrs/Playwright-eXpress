4988b79d0b854e14055afe4200f74c7f8baa950b
ruby --version <paulinn.rs@gmail.com>
2024-01-11 14:09:53 -0300

    Dados dinamicos com Faker

--- tests/tasks.spec.ts
+++ tests/tasks.spec.ts
@@ -1,9 +1,10 @@
 import { test, expect } from '@playwright/test'
+import { faker } from '@faker-js/faker'
 
 test(' deve poder cadastrar uma nova tarefa ', async ({ page }) => {
     await page.goto('http://localhost:3000')
     const inputTaskName = page.locator('input[class*=listInputNewTask]')
-    await inputTaskName.fill('Ler um livro de TypeScript')
+    await inputTaskName.fill(faker.lorem.words())
     await page.click('css=button >> text=Create')
 
 })
