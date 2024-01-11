bb9ebd90bd2535c0cffaf3809854b843fd032001
ruby --version <paulinn.rs@gmail.com>
2024-01-10 21:13:04 -0300

    Submetendo formulários HTML

--- tests/tasks.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test'
-
-test(' deve poder cadastrar uma nova tarefa ', async ({ page }) => {
-    await page.goto('http://localhost:3000')
-    await page.fill('input[class*=listInputNewTask]', 'Ler um livro de TypeScript')
-})
