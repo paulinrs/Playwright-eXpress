60de1b1f4a46c290a1034bdc10f54b9197caddcb
ruby --version <paulinn.rs@gmail.com>
2024-01-16 14:29:17 -0300

    Validando o comportamento esperado

--- tests/tasks.spec.ts
+++ tests/tasks.spec.ts
@@ -3,7 +3,6 @@ import { test, expect } from '@playwright/test'
 test(' deve poder cadastrar uma nova tarefa ', async ({ page, request }) => {
 
     const taskName = 'Ler um livro de TypeScript'
-    
     await request.delete('http://localhost:3333/helper/tasks/' + taskName)
 
     await page.goto('http://localhost:3000')
@@ -12,5 +11,8 @@ test(' deve poder cadastrar uma nova tarefa ', async ({ page, request }) => {
     await inputTaskName.fill(taskName)
     
     await page.click('css=button >> text=Create')
+
+    const target = page.locator(`css=.task-item p >> text=${taskName}`)
+    await expect(target).toBeVisible()
 
 })
