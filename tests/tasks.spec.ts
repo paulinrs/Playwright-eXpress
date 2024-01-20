4542060da180de9bcd30366a46d8e547f9a4b52f
ruby --version <paulinn.rs@gmail.com>
2024-01-20 14:52:26 -0300

    Testes independentes

--- tests/tasks.spec.ts
+++ tests/tasks.spec.ts
@@ -3,16 +3,41 @@ import { test, expect } from '@playwright/test'
 test(' deve poder cadastrar uma nova tarefa ', async ({ page, request }) => {
 
     const taskName = 'Ler um livro de TypeScript'
+
     await request.delete('http://localhost:3333/helper/tasks/' + taskName)
 
     await page.goto('http://localhost:3000')
 
     const inputTaskName = page.locator('input[class*=listInputNewTask]')
     await inputTaskName.fill(taskName)
-    
+
     await page.click('css=button >> text=Create')
 
     const target = page.locator(`css=.task-item p >> text=${taskName}`)
     await expect(target).toBeVisible()
+
+})
+
+test('não deve permitir tarefa duplicada', async ({ page, request }) => {
+
+    const task = {
+        name: 'estudar Node.js',
+        is_done: false
+    }
+
+    await request.delete('http://localhost:3333/helper/tasks/' + task.name)
+
+    const newTask = await request.post('http://localhost:3333/tasks', { data: task })
+    expect(newTask.ok()).toBeTruthy()
+
+    await page.goto('http://localhost:3000')
+
+    const inputTaskName = page.locator('input[class*=listInputNewTask]')
+    await inputTaskName.fill(task.name)
+
+    await page.click('css=button >> text=Create')
+
+    const target = page.locator('.swal2-html-container')
+    await expect(target).toHaveText('Task already exists!')
 
 })
